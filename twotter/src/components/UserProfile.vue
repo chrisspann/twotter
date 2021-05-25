85b65a3e6eb558ae20b630b4f0619a4dba1115a5
chrisspann <spann.chris@gmail.com>
2021-05-25 13:43:15 +0100

    initial commit

---
+++ twotter/src/components/UserProfile.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="user-profile">
+    <div class="user-profile__user-panel">
+    <h1>{{user.username}} - {{fullName}}</h1>
+        <div class ="user-profile__follower_count">
+        <strong>Followers: </strong> {{followers}}
+        </div>
+    </div>
+</div>
+<button @click="followUser">Follow</button>
+</template>
+<script>
+export default {
+      name: 'UserProfile',
+ data(){
+   return {
+     followers: 0,
+     user: {
+       id: 1,
+       username: 'cs87',
+       firstName: 'Chris',
+       lastName: 'Spann',
+       email: 'spann.chris@gmail.com',
+       isAdmin: false
+     }
+   }
+ },
+ watch:{
+ followers(newFollowerCount, oldFollowerCount){
+   if (oldFollowerCount < newFollowerCount) {
+     console.log(`${this.user.username} has gained a follower!`)
+   }
+ }
+ },
+ computed: {
+   fullName() {
+    return `${this.user.firstName} ${this.user.lastName}`;
+   }
+ },
+ methods: {
+  followUser() {
+    this.followers++
+  }
+ },
+ mounted(){
+  this.followUser();
+ }
+}
+</script>
